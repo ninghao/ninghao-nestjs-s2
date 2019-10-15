3d544e9111831469aa2394f78c572d33578290d9
wanghao <117663444@qq.com>
2019-10-15 16:06:15 +0800

    Config standard provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerService } from './logger.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: LoggerService,
+      useClass: LoggerService,
+    },
+  ],
 })
 export class AppModule {}
