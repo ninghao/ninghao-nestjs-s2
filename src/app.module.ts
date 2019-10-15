9c53302e0a2556a18dc7932c436f2ec54de21d27
wanghao <117663444@qq.com>
2019-10-15 18:07:17 +0800

    Value provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerService } from './logger.service';
 
+const mockAppService = {
+  getHello() {
+    return '您好';
+  },
+};
+
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [
-    AppService,
+    {
+      provide: AppService,
+      useValue: mockAppService,
+    },
     {
       provide: LoggerService,
       useClass: LoggerService,
