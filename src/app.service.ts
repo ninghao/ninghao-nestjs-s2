c49ea32507137fff5fe2fdd74dc0fe05d2f579e3
wanghao <117663444@qq.com>
2019-10-15 16:02:00 +0800

    Define and inject service provider

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { LoggerService } from './logger.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly logger: LoggerService) {}
+
   getHello(): string {
+    this.logger.log('AppService: Hello World!');
     return 'Hello World!';
   }
 }
