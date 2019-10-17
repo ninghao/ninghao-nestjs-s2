494a5aca26df5da3063a7dd527ed6bd4acabda80
wanghao <117663444@qq.com>
2019-10-17 15:25:57 +0800

    Use forwardRef reslove circular dependency issue

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [forwardRef(() => UserModule)],
   controllers: [AuthController],
   providers: [AuthService],
 })
