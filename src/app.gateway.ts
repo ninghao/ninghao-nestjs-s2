838990acf4a774c11cba480992871f39ab399986
wanghao <117663444@qq.com>
2019-10-18 15:43:03 +0800

    Subscribe message

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,4 +1,14 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+} from '@nestjs/websockets';
 
 @WebSocketGateway(3001, { namespace: 'app' })
-export class AppGateway {}
+export class AppGateway {
+  @SubscribeMessage('events')
+  handleEvent(@MessageBody() data: string) {
+    console.log(data);
+    return 'Server: got it!';
+  }
+}
