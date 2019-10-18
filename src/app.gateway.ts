8aade9bc4ca1f978df3df1d693426807d8d7de84
wanghao <117663444@qq.com>
2019-10-19 07:38:08 +0800

    WebSocket server handle connection

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -2,10 +2,21 @@ import {
   WebSocketGateway,
   SubscribeMessage,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway(3001, { namespace: 'app' })
-export class AppGateway {
+export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log('Client connected.');
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected');
+  }
+
   @SubscribeMessage('events')
   handleEvent(@MessageBody() data: string) {
     console.log(data);
