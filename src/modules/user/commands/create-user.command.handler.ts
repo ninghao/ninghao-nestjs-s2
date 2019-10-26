43e37eda4c8d0adbdcf83ed6e8e3f042d5e6b0a6
wanghao <117663444@qq.com>
2019-10-26 13:10:34 +0800

    Events：发布事件（EventBus）

--- src/modules/user/commands/create-user.command.handler.ts
+++ src/modules/user/commands/create-user.command.handler.ts
@@ -1,11 +1,15 @@
-import { ICommandHandler, CommandHandler } from '@nestjs/cqrs';
+import { ICommandHandler, CommandHandler, EventBus } from '@nestjs/cqrs';
 import { CreateUserCommand } from './create-user.command';
+import { UserCreatedEvent } from '../events/user-created.event';
 
 @CommandHandler(CreateUserCommand)
 export class CreateUserCommandHandler
   implements ICommandHandler<CreateUserCommand> {
+  constructor(private readonly eventBus: EventBus) {}
+
   async execute(command: CreateUserCommand) {
     console.log(command);
+    this.eventBus.publish(new UserCreatedEvent(command.user));
     return command.user;
   }
 }
