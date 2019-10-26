5973101c314f853f70491fe5684c9bf44abce7d3
wanghao <117663444@qq.com>
2019-10-26 13:38:07 +0800

    Queries：执行查询（QueryBus）

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { CommandBus } from '@nestjs/cqrs';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateUserCommand } from './commands/create-user.command';
+import { GetUsersQuery } from './queries/get-users.query';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
 
   @Post()
   createUser(@Body() user: any) {
     return this.commandBus.execute(new CreateUserCommand(user));
+  }
+
+  @Get()
+  getUsers() {
+    return this.queryBus.execute(new GetUsersQuery());
   }
 }
